05ac8d06a9fedb88c020576c0b040ce74b50c540
dexter2206 <dexter2206@gmail.com>
2020-02-17 18:09:54 +0100

    Add pandas to requiremets

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     entry_points={
         "console_scripts": ["omnisolver=omnisolver.cmd:main"]
     },
-    install_requires=["dimod", "typing_extensions", "pyaml"],
+    install_requires=["dimod", "typing_extensions", "pyaml", "pandas"],
     tests_require=["pytest"],
     packages=find_packages(exclude=["tests"]),
     package_data={"omnisolver": ["specifications/*.yml"]}
